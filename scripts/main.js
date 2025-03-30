29f23222cd6081b27d81721beb8bb1b96029673b
FES001 <firdavs7794948@gmail.com>
2025-03-30 20:42:35 +0500

    Working on it.

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+// WORD CONTAINERS
+const wordContainers = document.querySelectorAll('.main-word-inner');
+
+// KEYS
+const letterKeys = document.querySelectorAll('.main-keyboard-button-letter');
+const enterKey = document.querySelector('.main-keyboard-button-enter');
+const removeKey = document.querySelector('.main-keyboard-button-remove');
+
+// WORDLE
+const wordle = {
+    try: {
+        tryIndex: 0,
+        tryParentIndex: 0,
+        tryChildIndex: 0,
+        tryArr: [],
+    },
+};
+
+// HANDLING THE KEYS' FUNCTIONALITIES
+
+for (let i = 0; i < letterKeys.length; i++) {
+    letterKeys[i].addEventListener('click', () => {
+        if (wordle.try.tryArr.length < 5) {
+            wordContainers[wordle.try.tryParentIndex].children[wordle.try.tryChildIndex].textContent = letterKeys[i].value;
+            wordContainers[wordle.try.tryParentIndex].children[wordle.try.tryChildIndex].classList.add('main-word-inner-itself-occupied');
+            wordle.try.tryArr.push(letterKeys[i].value);
+            wordle.try.tryChildIndex++;
+        };
+    });
+};
